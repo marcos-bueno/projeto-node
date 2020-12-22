e7f5bb48997219da3696a45d3a94563722ffab0c
Marcos <mabueno@protonmail.com>
2020-12-22 16:35:46 -0300

    updating avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,6 +6,8 @@ import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -43,8 +45,27 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json({ user: userWithoutPassword });
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
