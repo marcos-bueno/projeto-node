adc11c5c9da8647b82d203eea7e59b1c1fbe8726
Marcos <mabueno@protonmail.com>
2020-12-22 11:10:19 -0300

    password encryption

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,17 +4,25 @@ import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
 
+interface User {
+  name: string;
+  email: string;
+  password?: string;
+}
+
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
 
     const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
+    const user: User = await createUser.execute({
       name,
       email,
       password,
     });
+
+    delete user.password;
 
     return response.json(user);
   } catch (err) {
